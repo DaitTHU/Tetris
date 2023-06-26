23989fd52733fa539ff64e396078a99d67ffc13b
DaitTHU <daiyj20@mails.tsinghua.edu.cn>
2023-06-26 20:48:43 +0800

    添加项目文件。

---
+++ Tetris/Tetris.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Tetris
+{
+    partial class Tetris
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Tetris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Tetris";
+            this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.Tetris_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Tetris/Tetris.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class Tetris : Form
+    {
+        private const uint UNIT = 35,
+            ROW = 20, COL = 10;
+
+
+        public Tetris()
+        {
+            InitializeComponent();
+        }
+
+        private void Tetris_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
