1de55463c266e5376933aaff9fb35aebb66f0613
DaitTHU <daiyj20@mails.tsinghua.edu.cn>
2023-06-29 10:14:12 +0800

    add check box

---
+++ Tetris/CheckBox.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Tetris
+{
+    partial class CheckBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.No = new System.Windows.Forms.Button();
+            this.Yes = new System.Windows.Forms.Button();
+            this.Info = new System.Windows.Forms.Label();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // No
+            // 
+            this.No.BackColor = System.Drawing.Color.DimGray;
+            this.No.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.No.ForeColor = System.Drawing.Color.White;
+            this.No.Location = new System.Drawing.Point(199, 111);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(99, 51);
+            this.No.TabIndex = 5;
+            this.No.Text = "No";
+            this.No.UseVisualStyleBackColor = false;
+            this.No.Click += new System.EventHandler(this.No_Click);
+            // 
+            // Yes
+            // 
+            this.Yes.BackColor = System.Drawing.Color.DimGray;
+            this.Yes.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Yes.ForeColor = System.Drawing.Color.White;
+            this.Yes.Location = new System.Drawing.Point(61, 111);
+            this.Yes.Name = "Yes";
+            this.Yes.Size = new System.Drawing.Size(99, 51);
+            this.Yes.TabIndex = 4;
+            this.Yes.Text = "Yes";
+            this.Yes.UseVisualStyleBackColor = false;
+            this.Yes.Click += new System.EventHandler(this.Yes_Click);
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Info.ForeColor = System.Drawing.Color.White;
+            this.Info.Location = new System.Drawing.Point(34, 49);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(305, 37);
+            this.Info.TabIndex = 3;
+            this.Info.Text = "0123456789ABCDEF";
+            this.Info.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OK
+            // 
+            this.OK.BackColor = System.Drawing.Color.DimGray;
+            this.OK.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OK.ForeColor = System.Drawing.Color.White;
+            this.OK.Location = new System.Drawing.Point(127, 111);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(99, 51);
+            this.OK.TabIndex = 6;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = false;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // CheckBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(366, 216);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Yes);
+            this.Controls.Add(this.Info);
+            this.Name = "CheckBox";
+            this.Text = "Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button No;
+        private System.Windows.Forms.Button Yes;
+        private System.Windows.Forms.Label Info;
+        private System.Windows.Forms.Button OK;
+    }
+}
---
+++ Tetris/CheckBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class CheckBox : Form
+    {
+        public CheckBox(string info, bool decision = true)
+        {
+            InitializeComponent();
+            //this.ClientSize = new Size(400, 300);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            //Info.Location = new Point(0, 50);
+            //Info.Size = new Size(this.ClientSize.Width, 50);
+            Info.Text = info.PadLeft(8 + info.Length / 2);
+            //Info.TextAlign = ContentAlignment.TopCenter;
+            Yes.Visible = decision;
+            No.Visible = decision;
+            OK.Visible = !decision;
+        }
+
+        private void Yes_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Yes;
+            Close();
+        }
+
+        private void No_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.No;
+            Close();
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+
+    public partial class InfoBox : CheckBox
+    {
+        public InfoBox(string info) : base(info, false)
+        { Text = "Info"; ShowDialog(); }
+    }
+}
