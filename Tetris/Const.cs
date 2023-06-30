4663981094fc5d515b6c64bd41398833a7d80ed5
DaitTHU <daiyj20@mails.tsinghua.edu.cn>
2023-07-01 00:47:29 +0800

    opt Const

--- Tetris/Const.cs
+++ Tetris/Const.cs
@@ -6,22 +6,22 @@ namespace Tetris
 {
     public static class Const
     {
-        public const int UNIT = 35,
-            COL = 10, ROW = 20, CELL = 4,
-            lineWidth = 1;
+        private const int UNIT = 35;
+        public const int COL = 10, ROW = 20, CELL = 4;
+        public const int lineWidth = 1;
         internal static readonly Color gridColor = Color.DimGray,
             lineColor = Color.White;
         internal static readonly Pen penGrid = new Pen(gridColor, lineWidth),
             pen = new Pen(lineColor, lineWidth);
-        internal static readonly SolidBrush brushHint = new SolidBrush(gridColor),
+        internal static readonly SolidBrush brushGhost = new SolidBrush(gridColor),
             brushLine = new SolidBrush(lineColor),
             eraser = new SolidBrush(Color.Transparent);
         internal static readonly Random random = new Random();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static int Unit(int i) => i * UNIT;
+        internal static int Unit(int i = 1) => i * UNIT;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int IntSq(int x) => x * x;
+        public static int Pow2(int x) => x * x;
     }
 }
